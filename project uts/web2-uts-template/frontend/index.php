756cf38aaec03c6f217bed51e036b1b3906e425b
haexxzln02 <115056456+azleeny@users.noreply.github.com>
2023-05-16 00:00:41 +0700

    Add files via upload

---
+++ project uts/web2-uts-template/frontend/index.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta
+			name="viewport"
+			content="width=device-width, initial-scale=1, shrink-to-fit=no"
+		/>
+		<meta name="description" content="" />
+		<meta name="author" content="" />
+		<title>Shop Homepage - Start Bootstrap Template</title>
+		<!-- Favicon-->
+		<link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+		<!-- Bootstrap icons-->
+		<link
+			href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"
+			rel="stylesheet"
+		/>
+		<!-- Core theme CSS (includes Bootstrap)-->
+		<link href="css/styles.css" rel="stylesheet" />
+	</head>
+	<body>
+		<!-- Navigation-->
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<div class="container px-4 px-lg-5">
+				<a class="navbar-brand" href="#!">azln's web</a>
+				<button
+					class="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+						<li class="nav-item">
+							<a class="nav-link active" aria-current="page" href="#!">Home</a>
+						</li>
+						<li class="nav-item"><a class="nav-link" href="#!">About</a></li>
+						<li class="nav-item dropdown">
+							<a
+								class="nav-link dropdown-toggle"
+								id="navbarDropdown"
+								href="#"
+								role="button"
+								data-bs-toggle="dropdown"
+								aria-expanded="false"
+								>Shop</a
+							>
+							<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+								<li><a class="dropdown-item" href="#!">All Products</a></li>
+								<li><hr class="dropdown-divider" /></li>
+								<li><a class="dropdown-item" href="#!">Popular Items</a></li>
+								<li><a class="dropdown-item" href="#!">New Arrivals</a></li>
+							</ul>
+						</li>
+					</ul>
+					<form class="d-flex">
+						<button class="btn btn-outline-dark" type="submit">
+							<i class="bi-cart-fill me-1"></i>
+							Cart
+							<span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+						</button>
+					</form>
+				</div>
+			</div>
+		</nav>
+		<!-- Header-->
+		<header class="bg-dark py-5">
+			<div class="container px-4 px-lg-5 my-5">
+				<div class="text-center text-white">
+					<h1 class="display-4 fw-bolder">Azleeny's stationery store</h1>
+					<p class="lead fw-normal text-white-50 mb-0">Menjual alat Tulis dsb</p>
+				</div>
+			</div>
+		</header>
+		<!-- Section-->
+		<section class="py-5">
+			<div class="container px-4 px-lg-5 mt-5">
+				<div
+					class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center"
+				>
+					<div class="col mb-5">
+						<div class="card">
+							<!-- Sale badge-->
+							<div
+								class="badge bg-dark text-white position-absolute"
+								style="top: 0.5rem; right: 0.5rem"
+							>
+								Sale
+							</div>
+							<!-- Product image-->
+							<img
+								class="card-img-top"
+								src="http://primajayastationery.com/wp-content/uploads/2018/04/1767082_d04ce4a7-b67d-4620-b08d-cf03bf19ba6c-230x250.png"
+								alt="..."
+							/>
+							<!-- Product details-->
+							<div class="card-body p-4">
+								<div class="text-center">
+									<!-- Product name-->
+									<h5 class="fw-bolder">Printer Canon Pixma G2000 Print Copy Scan</h5>
+									<!-- Product price-->
+									<span class="text-muted text-decoration-line-through"
+										>Rp 2.500.000</span>
+									Rp 2.250.000
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col mb-5">
+						<div class="card">
+							<!-- Sale badge-->
+							<div
+								class="badge bg-dark text-white position-absolute"
+								style="top: 0.5rem; right: 0.5rem"
+							>
+								Sale
+							</div>
+							<!-- Product image-->
+							<img
+								class="card-img-top"
+								src="http://primajayastationery.com/wp-content/uploads/2017/02/HP-49A-Black-Original-LaserJet-Toner-Cartridge-Q5949A-350x333.png"
+								alt="..."
+							/>
+							<!-- Product details-->
+							<div class="card-body p-4">
+								<div class="text-center">
+									<!-- Product name-->
+									<h5 class="fw-bolder">HP 49A Black Original LaserJet Toner Cartridge – Q5949A</h5>
+									<!-- Product price-->
+									<span class="text-muted text-decoration-line-through"
+										>Rp 9.00.000</span
+									>
+									Rp 850.000
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col mb-5">
+						<div class="card">
+							<!-- Sale badge-->
+							<div
+								class="badge bg-dark text-white position-absolute"
+								style="top: 0.5rem; right: 0.5rem"
+							>
+								Sale
+							</div>
+							<!-- Product image-->
+							<img
+								class="card-img-top"
+								src="http://primajayastationery.com/wp-content/uploads/2019/03/Rak-kertas-besi-4-susun-Tray-besi-4-susunA-350x350.jpg"
+								alt="..."
+							/>
+							<!-- Product details-->
+							<div class="card-body p-4">
+								<div class="text-center">
+									<!-- Product name-->
+									<h5 class="fw-bolder">Rak kertas besi 4 susun / Tray besi 4 susun</h5>
+									<!-- Product price-->
+									<span class="text-muted text-decoration-line-through"
+										>Rp 170.000</span
+									>
+									Rp 160.000
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col mb-5">
+						<div class="card">
+							<!-- Sale badge-->
+							<div
+								class="badge bg-dark text-white position-absolute"
+								style="top: 0.5rem; right: 0.5rem"
+							>
+								Sale
+							</div>
+							<!-- Product image-->
+							<img
+								class="card-img-top"
+								src="https://images-na.ssl-images-amazon.com/images/I/51ss1Z1YG9L._SL500_._AC_SL500_.jpg"
+								alt="..."
+							/>
+							<!-- Product details-->
+							<div class="card-body p-4">
+								<div class="text-center">
+									<!-- Product name-->
+									<h5 class="fw-bolder">Faber-Castell Connector Felt Pen 20 Color, F155520</h5>
+									<!-- Product price-->
+									<span class="text-muted text-decoration-line-through"
+										>Rp 125.000</span
+									>
+									Rp 100.000
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Footer-->
+		<footer class="py-5 bg-dark">
+			<div class="container">
+				<p class="m-0 text-center text-white">
+					Copyright &copy; Your Website 2023
+				</p>
+			</div>
+		</footer>
+		<!-- Bootstrap core JS-->
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+		<!-- Core theme JS-->
+		<script src="js/scripts.js"></script>
+	</body>
+</html>
